3f34ca8c3a7e0bf511a3a86228dd2e417fe2a808
Jessica Hsu <23249301+hsujessica@users.noreply.github.com>
2018-02-18 23:24:06 -0500

    added initial components

---
+++ src/components/Playground.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import {Component} from 'react';
+
+
+export class Playground extends Component {
+  render() {
+    return (
+      <div className="playground">
+        <h2>MINDFLO PLAYGROUND</h2>
+      </div>
+    );
+  }
+}
+
+export default Playground;
